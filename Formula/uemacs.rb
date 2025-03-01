dd0a0406281c62da305143397fe9761da2819458
Davide Restivo <davide.restivo@yahoo.it>
2025-03-01 18:19:59 +0100

    Add Formula

---
+++ Formula/uemacs.rb
@@ -0,0 +1,15 @@
+# coding: utf-8
+class Uemacs < Formula
+  desc "MicroEMACS modified by Linus Torvalds"
+  homepage "https://github.com/torvalds/uemacs"
+  url "https://github.com/torvalds/uemacs.git"
+  version "4.0"
+
+  depends_on "cmake" => :build
+  depends_on "gcc"   => :build
+
+  def install
+    system "make"
+    bin.install "em"
+  end
+end
